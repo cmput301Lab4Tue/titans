85a9a876ed1054f718bdaf3180c9760c60dfa356
jjfedun <jjfedun@ualberta.ca>
2024-09-24 18:35:39 -0600

    Shape class added

---
+++ app/src/main/java/com/example/titans/Shape.java
@@ -0,0 +1,26 @@
+package com.example.titans;
+
+public abstract class Shape {
+
+    private Integer x;
+    private Integer y;
+
+    public Shape (Integer x, Integer y){
+        this.x = x;
+        this.y = y;
+    }
+    public Integer getX() {
+        return this.x;
+    }
+
+    public Integer getY() {
+        return this.y;
+    }
+
+    public void setX (Integer x){
+        this.x = x;
+    }
+    public void setY (Integer y){
+        this.y = y;
+    }
+}
