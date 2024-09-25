cd6c8501df565c8da0de99cf651ed2f54c871172
jjfedun <jjfedun@ualberta.ca>
2024-09-24 18:59:34 -0600

    Square added

---
+++ app/src/main/java/com/example/titans/Square.java
@@ -0,0 +1,13 @@
+package com.example.titans;
+
+public class Square extends Shape{
+    public Square(Integer x) {
+        super(x, x);
+    }
+    public String getShape(){
+        return "Square";
+    }
+    public Integer getSize(){
+        return getX();
+    }
+}
