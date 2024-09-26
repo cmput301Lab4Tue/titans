cd59dd173d299d97c5a93a235a7e700c731ebf13
Incerdi- <ab.erinwusi@gmail.com>
2024-09-26 17:36:00 -0600

    Added string color in Shape on the main branch
    # Conflicts:
    #	app/src/main/java/com/example/titans/Shape.java

--- app/src/main/java/com/example/titans/Shape.java
+++ app/src/main/java/com/example/titans/Shape.java
@@ -4,12 +4,10 @@ public abstract class Shape {
 
     private Integer x;
     private Integer y;
-    private String color;
 
     public Shape (Integer x, Integer y){
         this.x = x;
         this.y = y;
-        this.color = color;
     }
     public Integer getX() {
         return this.x;
@@ -17,11 +15,6 @@ public abstract class Shape {
 
     public Integer getY() {
         return this.y;
-    }
-
-    public String getColor() {
-        String color = "Blue";
-        return this.color;
     }
 
     public void setX (Integer x){
