0ec450b73aacdf6f1f82355c3f743ad45f4947ff
Joe <90614259+Joe1742@users.noreply.github.com>
2024-09-27 15:06:30 -0600

    Update Shape.java

--- app/src/main/java/com/example/titans/Shape.java
+++ app/src/main/java/com/example/titans/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
 
     private Integer x;
     private Integer y;
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     public Shape (Integer x, Integer y){
         this.x = x;
